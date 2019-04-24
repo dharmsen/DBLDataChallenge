5de4764ba731edb3b0a092ccb1691f202b54a591
LourensT <touwenlourens@gmail.com>
2019-04-24 21:45:45 +0200

    extra formatting

--- dates_string.py
+++ dates_string.py
@@ -7,12 +7,15 @@ DATA_FILEPATH = "C://Users/20181884//Documents//Y1Q4/DBL//airlines_complete"
 
 dates = []
 dates_string = []
+file_names = []
 for item in os.listdir(DATA_FILEPATH):
     epoch = int(float(item.replace(".json", "").replace("airlines-", ""))/1000)
+
+    file_names.append(item)
     dates.append(datetime.datetime.fromtimestamp(epoch))
     dates_string.append(datetime.datetime.fromtimestamp(epoch).strftime('%Y-%m-%d'))
 
-with open('dates_string.txt', 'w', encoding='utf8') as output:
+with open('dates_string.csv', 'w', encoding='utf8') as output:
     writer = csv.writer(output, delimiter=',')
-    for item in dates_string:
-        writer.writerow([item,])
+    for row in zip(file_names, dates, dates_string):
+        writer.writerow(row)
