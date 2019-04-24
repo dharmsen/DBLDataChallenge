f0cc135cfd32ca7509a2f279d13271da19ccaf8f
LourensT <touwenlourens@gmail.com>
2019-04-24 22:01:07 +0200

    put shit in functions because modularity yeehaw

--- dates_string.py
+++ dates_string.py
@@ -5,17 +5,25 @@ import csv
 
 DATA_FILEPATH = "C://Users/20181884//Documents//Y1Q4/DBL//airlines_complete"
 
-dates = []
-dates_string = []
-file_names = []
-for item in os.listdir(DATA_FILEPATH):
-    epoch = int(float(item.replace(".json", "").replace("airlines-", ""))/1000)
+def getDates(fp):
+    dates = []
+    dates_string = []
+    file_names = []
+    for item in os.listdir(DATA_FILEPATH):
+        epoch = int(float(item.replace(".json", "").replace("airlines-", ""))/1000)
 
-    file_names.append(item)
-    dates.append(datetime.datetime.fromtimestamp(epoch))
-    dates_string.append(datetime.datetime.fromtimestamp(epoch).strftime('%Y-%m-%d'))
+        file_names.append(item)
+        dates.append(datetime.datetime.fromtimestamp(epoch))
+        dates_string.append(datetime.datetime.fromtimestamp(epoch).strftime('%Y-%m-%d'))
 
-with open('dates_string.csv', 'w', encoding='utf8', newline="") as output:
-    writer = csv.writer(output, delimiter=',')
-    for row in zip(file_names, dates, dates_string):
-        writer.writerow(row)
+    return file_names, dates_string
+
+def saveFile(file_names, dates_string):
+    with open('dates_string.csv', 'w', encoding='utf8', newline="") as output:
+        writer = csv.writer(output, delimiter=',')
+        for row in zip(file_names, dates_string):
+            writer.writerow(row)
+
+if __name__ == '__main__':
+    a, b = getDates(DATA_FILEPATH)
+    saveFile(a, b)
