8ed1316da2d44d1db0d563ee0fa58fc94ba46063
clepelaars <info@carlolepelaars.nl>
2019-04-25 14:57:10 +0200

    Built JSON to dataframe extractor

--- conversation_data_extractor.py
+++ conversation_data_extractor.py
@@ -3,6 +3,10 @@ import pandas as pd
 
 
 class conversation_extractor:
+    '''
+    Converts a single JSON file to DataFrame with
+    specified features
+    '''
     def __init__(self, path, features):
         self.path = path
         self.features = features
@@ -15,40 +19,31 @@ class conversation_extractor:
         out of a JSON file
         '''
         for line in open(self.path, mode='r'):
-            yield json.loads(line)
-
-    # def read_tweets(self):
-    #     tweets = []
-    #     for tweet in self.generator:
-    #         tweets.append(type(tweet))
-    #     return tweets
-
-    # def get_columns(self, features):
-    #     list_of_lists = []
-    #     for feat in features:
-    #         feature_list = []
-    #         for item in self.generator:
-    #             feature_list.append(item[feat])
-    #         list_of_lists.append(feature_list)
-    #     return list_of_lists
+            try:
+                yield json.loads(line)
+            except json.decoder.JSONDecodeError:
+                # for now
+                pass
 
     def add_content(self):
+        '''
+        Creates a list of lists for constructing
+        the dataframe
+        '''
         rows = []
-        for _ in self.generator:
-            rows.append(self.features)
+        for row in self.generator:
+            try:
+                rows.append([row[x] for x in self.features])
+            except KeyError:
+                pass
         return rows
 
-    def make_dataframe(self, content=[]):
-        dataframe = pd.DataFrame(columns=self.features)
-        if len(content) == 0:
-            pass
-        else:
-            assert len(content) == len(self.features), 'length of content and features are not equal'
-            content = self.add_content()
-            for row in content:
-                dataframe.append(row)
+    def make_dataframe(self):
+        '''
+        Combines the features and content into a dataframe
+        '''
+        content = self.add_content()
+        dataframe = pd.DataFrame(content, columns=self.features)
         return dataframe
 
 
-#if __name__ == '__main__':
-#    pass
