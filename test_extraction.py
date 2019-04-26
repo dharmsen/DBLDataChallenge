9d3067f55a4a23b1b5d017f677b1ca128f91f36e
Carlo Lepelaars <info@carlolepelaars.nl>
2019-04-26 18:53:49 +0200

    Testing with multiple JSON files

--- test_extraction.py
+++ test_extraction.py
@@ -1,8 +1,11 @@
+import os
 from conversation_data_extractor import conversation_extractor
 from DataBaseInterface import SaveDataFrameAsDB, LoadDatabaseAsDF
 
 if __name__ == '__main__':
-    extractor = conversation_extractor('test_file.json', features=['id_str', 'text',
+    directory = 'test_directory/'
+    items = os.listdir(directory)
+    extractor = conversation_extractor(directory=directory, features=['id_str', 'text',
                                                                    'lang', 'created_at',
                                                                    'in_reply_to_status_id',
                                                                    'in_reply_to_user_id',
@@ -14,10 +17,10 @@ if __name__ == '__main__':
     print('\n')
 
     print('dumping dataframe in database test_file.db')
-    SaveDataFrameAsDB(dataframe, filename='test_file.db')
+    SaveDataFrameAsDB(dataframe, filename='test_file2.db')
 
     print('\n')
     print('loading dataframe again as sanitycheck')
-    df2 = LoadDatabaseAsDF('test_file.db')
+    df2 = LoadDatabaseAsDF('test_file2.db')
     print(df2.head(2))
     print(f'Shape: {df2.shape}')
