348156b1635a7b3f6fcea201844820ce2ab3701e
LourensT <touwenlourens@gmail.com>
2019-04-30 16:42:22 +0200

    commit 1

---
+++ AddConvoTable.py
@@ -0,0 +1,31 @@
+from DataBaseInterface import LoadDatabaseAsDF
+import pandas as pd
+
+df = LoadDatabaseAsDF('10_jsons.db')
+parent_tweetid = df[df['in_reply_to_status_id'].isna()]['id_str']
+parent_userid = df[df['in_reply_to_status_id'].isna()]['(\'user\', \'id_str\')']
+
+print(df.shape)
+
+df_final = pd.DataFrame(index=parent_tweetid, columns=['parent_user_id', 'responses'])
+df_final.reset_index(level=0, inplace=True)
+df_final['parent_user_id'] = parent_userid
+print(df_final.shape)
+
+df_replies = df[~df['in_reply_to_status_id'].isna()]
+
+for item in list(df_replies['in_reply_to_status_id']):
+    #print(item)
+    #print(list(df_final['id_str']))
+    if item in list(df_final['id_str']):
+        print('hey')
+        tweet_entry = (df_replies[df_replies['in_reply_to_status_id']==item]['id_str'],
+            df_replies[df_replies['in_reply_to_status_id']==item]['(\'user\', \'id_str\')'],
+            df_replies[df_replies['in_reply_to_status_id']==item]['in_reply_to_status_id'])
+        current_response = df_final['responses'][item]
+        current_response.append(tweet_entry)
+        df_final.at['item', current_response]
+    else:
+        print('parent not in there')
+
+print(df_final[~df_final['responses'].isna()].head(5))
