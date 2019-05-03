d26364242a087616ca05309a59ef4298aab85795
LourensT <touwenlourens@gmail.com>
2019-05-03 09:38:10 +0200

    bugfixes

--- LinearConversations.py
+++ LinearConversations.py
@@ -1,9 +1,10 @@
 from DataBaseInterface import LoadDatabaseAsDF
 import pandas as pd
 import csv
-import datetime
+from datetime import datetime
+import numpy as np
 
-df = LoadDatabaseAsDF('10_jsons.db', ['tweets',])[0]
+df = LoadDatabaseAsDF('full_conversation_database.db', ['tweets',])[0]
 print(df.shape)
 
 def TweetEntry(row):
@@ -12,23 +13,34 @@ def TweetEntry(row):
 
     return (tweetid, userid)
 
+def SaveProgress(conversations_dict, wanted_dict, id):
+    fp = 'full_db_conversations_' + str(id) + '.csv'
+    with open(fp, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        for key, value in conversations_dict.items():
+           writer.writerow([key, value])
+
+    fp2 = 'wanted_dict_at_'+str(id)+'.csv'
+    with open(fp2, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        for key, value in wanted_dict.items():
+           writer.writerow([key, value])
+
 conversations = {}
-conversations_count = 0
 wanted = {}
 startTime = datetime.now()
+error_count = 0
 
 for item, row in df[::-1].iterrows():
     if ((int(item) % 10000) == 0):
         print('At row ' + str(item))
-        print('There are ' + str(conversation_id) + 'conversations so far')
-        print('There are ' + str(len(conversations)) + 'wanted tweets so far')
+        print('There are ' + str(conversation_id) + ' conversations so far')
+        print('There are ' + str(len(wanted)) + ' wanted tweets so far')
         print('running time.. ' + str(datetime.now() - startTime))
         print('=============================================================')
-    if ((int(item) % 1000000) == 0):
-        with open('full_db_conversations.csv', 'w', newline='') as csv_file:
-            writer = csv.writer(csv_file)
-            for key, value in conversations.items():
-               writer.writerow([key, value])
+        SaveProgress(conversations, wanted, item)
+    if ((int(item) % 500000) == 0):
+        SaveProgress(conversations, wanted, item)
 
     if int(row['id_str']) in wanted:
         conversation_id = wanted[int(row['id_str'])]
@@ -36,19 +48,25 @@ for item, row in df[::-1].iterrows():
             current_entry = conversations[conversation_id]
             current_entry.append(TweetEntry(row))
             conversations[conversation_id] = current_entry
-            print('list: ' + str(conversation_id) + ' ' + str(conversations[conversation_id]))
             del wanted[int(row['id_str'])]
         except AttributeError:
             print('nonetype error at ' + str(item))
+            error_count += 1
 
-        if isinstance(row['in_reply_to_status_id'], float):
-            wanted[row['in_reply_to_status_id']] = conversation_id
-    elif isinstance(row['in_reply_to_status_id'], float):
+        if not np.isnan(row['in_reply_to_status_id']):
+                print(row['in_reply_to_status_id'])
+                wanted[row['in_reply_to_status_id']] = conversation_id:
+    elif not np.isnan(row['in_reply_to_status_id']):
+        print(row['in_reply_to_status_id'])
+        conversations_count = len(conversations)
         conversations[conversations_count] = [TweetEntry(row),]
-        wanted[row['in_reply_to_status_id']] = conversations_count
-        conversations_count += 1
+        wanted[int(row['in_reply_to_status_id')]] = conversations_count
 
-with open('full_db_conversations.csv', 'w', newline='') as csv_file:
-    writer = csv.writer(csv_file)
-    for key, value in conversations.items():
-       writer.writerow([key, value])
+SaveProgress(conversations, wanted, 'final')
+
+print('At row ' + str(item))
+print('There are ' + str(conversation_id) + 'conversations FINAL')
+print('There are ' + str(len(conversations)) + 'wanted tweets FINAL')
+print('running time.. ' + str(datetime.now() - startTime))
+print('=============================================================')
+print('error count ' + str(error_count))
