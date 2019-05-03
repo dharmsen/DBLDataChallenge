e67482493297a2b19e7cf9fed3bdfde99e8afb6a
clepelaars <info@carlolepelaars.nl>
2019-05-03 11:43:27 +0200

    Added wrangled csv file of full_db_conversations and uploaded conversation_wrangling_script

---
+++ conversation_wrangling_script.py
@@ -0,0 +1,134 @@
+# Dependencies
+import time
+import pandas as pd
+
+class ConversationWrangler:
+    '''Performs filtering and feature
+    engineering steps on airline conversation data.
+    '''
+    def __init__(self, file_name):
+        '''
+            Takes a conversation csv file with the columns 'conversation_id' and 'raw_tweets_info'
+        '''
+        self.file_name = file_name
+        # Read csv in DataFrame
+        self.df = pd.read_csv(self.file_name, names=['conversation_id', 'raw_tweets_info'])
+
+    def conversation_length(self):
+        '''
+        Extracts conversation length for each conversation and adds them
+        to a new column
+        '''
+        self.df['conversation_length'] = [len(eval(tweet)) for tweet in list(self.df['raw_tweets_info'])]
+
+    def filter_conversations(self, min_length=2):
+        '''
+        Keeps only conversation which have a certain length
+        Take an argument 'min_length' which the denotes
+        the minimum length of conversations you want to keep
+        '''
+        self.df = self.df[self.df['conversation_length'] >= min_length]
+
+
+    def tweet_ids(self):
+        '''
+        Extracts a list of tweet ids from each conversation
+        and adds them to a new column.
+        '''
+        tweet_ids = []
+        # Collect list of lists with tweet ids from every conversation
+        for conv in list(self.df['raw_tweets_info']):
+            ids = []
+            for tweet in eval(conv):
+                ids.append(tweet[0])
+            # Append flipped version
+            tweet_ids.append(ids[::-1])
+
+        # Add tweet_ids feature to DataFrame
+        self.df['tweet_ids'] = tweet_ids
+
+    def user_ids(self):
+        '''.
+        Extracts a list of user ids from each conversation
+        and adds them to a new column.
+        '''
+        user_ids = []
+        # Collect list of lists with user ids from every conversation
+        for conv in list(self.df['raw_tweets_info']):
+            ids = []
+            for tweet in eval(conv):
+                ids.append(tweet[1])
+            # Append flipped version
+            user_ids.append(ids[::-1])
+
+        # Add user_ids feature to DataFrame
+        self.df['user_ids'] = user_ids
+
+    def airlines_involved(self):
+        '''
+        Checks which airlines are involved in a conversation
+        and adds those airlines to a new column.
+        '''
+        # Map airline ids and names
+        keys = ["56377143", "106062176", "18332190", "22536055", "124476322", "26223583", "2182373406", "38676903",
+                  "1542862735", "253340062", "218730857", "45621423", "20626359"]
+        values = ["KLM", "AirFrance", "British_Airways", "AmericanAir", "Lufthansa", "AirBerlin", "AirBerlin assist",
+                "easyJet", "RyanAir", "SingaporeAir", "Qantas", "EtihadAirways", "VirginAtlantic"]
+        keys = [int(key) for key in keys]
+        airline_dict = dict(zip(keys, values))
+
+        # Collect contents of airlines_involved feature
+        airlines = []
+        for row in list(self.df['user_ids']):
+            # Check if there are airlines involved in the conversation
+            if bool(set(list(row)).intersection(keys)):
+                # Collect the ids of the airlines
+                airline_ids = set((list(row))).intersection(keys)
+                # Extract airline names from these ids
+                extracted_airlines = [airline_dict[airline_id] for airline_id in airline_ids]
+                airlines.append(extracted_airlines)
+            else:
+                # Add placeholder
+                airlines.append(['No airlines involved'])
+
+        # Append feature to the DataFrame
+        self.df['airlines_involved'] = airlines
+
+    def to_csv(self, file_name=None):
+        '''
+        Saves the DataFrame to a csv file
+        '''
+        if file_name == None:
+            # Standard file_name
+            file_name = f'wrangled_{self.file_name}'
+        self.df.to_csv(file_name, index=False)
+
+    def to_table(self):
+        '''
+        Adds the DataFrame to an SQL table
+        '''
+        # TODO Make function that adds DataFrame to an SQL table
+        pass
+
+    def full_standard_wrangle(self, min_length=2, file_name=None):
+        '''
+        Performs all steps in the class
+        Takes an argument 'min_length' for filtering conversations
+        '''
+        self.conversation_length()
+        self.filter_conversations(min_length=min_length)
+        self.tweet_ids()
+        self.user_ids()
+        self.airlines_involved()
+        self.to_csv(file_name=file_name)
+
+if __name__ == '__main__':
+    t_start = time.time()
+    wrangler = ConversationWrangler('full_db_conversations_final.csv')
+    wrangler.full_standard_wrangle(min_length=2, file_name=None)
+    t_finish = time.time()
+    total_time = round((t_finish - t_start), 4)
+    print('Runtime = {} seconds'.format(total_time))
+
+
+
