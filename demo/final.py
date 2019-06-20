a992b5cf986f3ff1fe842bb6ce5228a7b63bdb78
clepelaars <info@carlolepelaars.nl>
2019-06-20 14:15:08 +0200

    Demo outline

--- demo/final.py
+++ demo/final.py
@@ -1,33 +1,27 @@
 # Import Stuff
-import pandas as pd
+# import pandas as pd
+
 
 class Demo:
     def __init__(self, directory):
         self.dir = directory
+        self.unzipper = ''
+        self.data_extractor = ''
+        self.sentiment_extractor = ''
+        self.date_extractor = ''
+        self.incoming_outcoming_extractor = ''
+        self.plotter = ''
 
-    def unzip(self):
+    def demo(self):
         pass
-
-    def extract_data(self):
-        pass
-
-    def add_sentiment(self):
-        pass
-
-    def add_dates(self):
-        pass
-
-    def get_incoming_outcoming(self):
-        pass
-
-    def make_plot(self):
-        pass
-
-    def run_demo():
-        pass
+        # self.unzipper.unzip_all()
+        # self.data_extractor.make_csv()
+        # self.sentiment_extractor.add_sentiment()
+        # self.date_extractor.extract_date()
+        # self.incoming_outcoming_extractor.extract()
+        # self.plotter.plot()
 
 
-
-
-if __name__ ==' __main__':
-    run_demo()
+if __name__ == ' __main__':
+    demo = Demo('demo')
+    demo.demo()
